955215b3d649bbedc040349180e69f8a8b90d8c5
Mounir El hattab <mounoumounou2021@gmail.com>
2026-08-24 12:55:16 +0100

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int left = 0;
+        int right = 1;
+
+        int result = 0;
+
+        while(right<prices.length){
+            int profit = 0;
+            if(prices[right] > prices[left]){
+                profit = prices[right] - prices[left];
+                result = Math.max(result, profit);
+            }else{
+                left = right;
+            }
+            right++;
+        }
+
+        return result;
+    }
+
+}
